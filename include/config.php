3d2d818eb72587f39f08bcd6080bf2ca810a57f1
dev.mhrony <mh.rony.dhanvi@gmail.com>
2023-10-07 17:32:54 +0600

    Last Update - 07/10/2023

--- include/config.php
+++ include/config.php
@@ -4,7 +4,7 @@ ob_start();
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','gymdb');
+define('DB_NAME','Your_Database_Name');
 // Establish database connection.
 try
 {
