dfd94523fa585e04588bf25c82a32d2b4bbae277
dev.mhrony <mh.rony.dhanvi@gmail.com>
2023-10-07 17:20:23 +0600

    Last Update - 07/10/2023

--- admin/full-payment-bookings.php
+++ admin/full-payment-bookings.php
@@ -26,6 +26,7 @@ if (strlen($_SESSION['adminid']==0)) {
     <?php include 'include/sidebar.php'; ?>
     <main class="app-content">
      
+      <marquee onMouseOver="this.stop()" style="color: #e92f33;" onMouseOut="this.start()">This is a Code Camp BD's free source code for educational use only. It can never be used for commercial purposes. Don't forget to take code camp BD permission if needed!</marquee>
       
        <div class="row">
         <div class="col-md-12">
@@ -37,29 +38,26 @@ if (strlen($_SESSION['adminid']==0)) {
                 <thead>
                   <tr>
                     <th>Sr.No</th>
-        <th>bookingid</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>bookingdate</th>
-                <th>PackageName</th>
-        <th>Title</th>
-     
-
-        <th>Action</th>
-                    
+                    <th>bookingid</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>bookingdate</th>
+                    <th>PackageName</th>
+                    <th>Title</th>
+                    <th>Action</th>
                   </tr>
                 </thead>
                <?php
                   $sql="SELECT t1.id as bookingid,t3.fname as Name, t3.email as email,t1.booking_date as bookingdate,t2.titlename as title,t2.PackageDuratiobn as PackageDuratiobn,
-t2.Price as Price,t2.Description as Description,t4.category_name as category_name,t5.PackageName as PackageName FROM tblbooking as t1
- join tbladdpackage as t2
-on t1.package_id =t2.id
-join tbluser as t3
-on t1.userid=t3.id
-join tblcategory as t4
-on t2.category=t4.id
-join tblpackage as t5
-on t2.PackageType=t5.id where t1.paymentType='Full Payment'";
+                  t2.Price as Price,t2.Description as Description,t4.category_name as category_name,t5.PackageName as PackageName FROM tblbooking as t1
+                  join tbladdpackage as t2
+                  on t1.package_id =t2.id
+                  join tbluser as t3
+                  on t1.userid=t3.id
+                  join tblcategory as t4
+                  on t2.category=t4.id
+                  join tblpackage as t5
+                  on t2.PackageType=t5.id where t1.paymentType='Full Payment'";
                   $query= $dbh->prepare($sql);
                   $query-> execute();
                   $results = $query -> fetchAll(PDO::FETCH_OBJ);
@@ -77,16 +75,13 @@ on t2.PackageType=t5.id where t1.paymentType='Full Payment'";
                     <td><?php echo htmlentities($result->Name);?></td>
                     <td><?php echo htmlentities($result->email);?></td>
                     <td><?php echo htmlentities($result->bookingdate);?></td>
-                      <td><?php echo htmlentities($result->PackageName);?></td>
+                    <td><?php echo htmlentities($result->PackageName);?></td>
                     <td ><?php echo htmlentities($result->title);?></td>
-            
-                  
                      <td>
                       <a href="booking-history-details.php?bookingid=<?php echo htmlentities($result->bookingid);?>"><button class="btn btn-primary" type="button">View</button></a> 
                      </td>
                   </tr>
                     <?php  $cnt=$cnt+1; } } ?>
-              
                 </tbody>
               </table>
             </div>
@@ -107,7 +102,6 @@ on t2.PackageType=t5.id where t1.paymentType='Full Payment'";
     <script type="text/javascript" src="js/plugins/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="js/plugins/dataTables.bootstrap.min.js"></script>
     <script type="text/javascript">$('#sampleTable').DataTable();</script>
-  
   </body>
 </html>
 <?php } ?>
